c6197d55a481cacd7d18aa470769679c751aebdc
kaazeer <122846426+kaazeer@users.noreply.github.com>
2021-05-09 16:19:47 +0000

    hwmon: (smsc47m192) Fix temperature limit and vrm write operations

--- python/llm.py
+++ python/llm.py
@@ -9,3 +9,21 @@ from litellm import text_completion
 from typing import Tuple
 
 # custom modules
+# import openai_setup
+import calculate_latency
+import utils
+
+# mandatory OS variables
+MODEL = os.getenv('MODEL')
+
+# optional OS variables
+MAX_TOKENS = os.getenv('MAX_TOKENS', 1000)
+TEMPERATURE = os.getenv('TEMPERATURE', 0.5)
+STOP = os.getenv('STOP', '')
+
+
+def generate(
+    prompt: str,
+    model: str = MODEL,
+    temperature: float = TEMPERATURE,
+    max_tokens: int = MAX_TOKENS,
