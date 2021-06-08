8ca7bf36ba5f80a4cc8f1daabfcbe6a7780729f4
kaazeer <122846426+kaazeer@users.noreply.github.com>
2021-06-08 12:54:31 +0000

    add CORS support

--- python/utils.py
+++ python/utils.py
@@ -17,4 +17,4 @@ def help():
         # 'Commands:',
         # 'PrompterSetup    - read OS environment variables',
         # 'PrompterGenerate - trigger the prompt completion',
-        # 'PrompterInfo     - some informati
+        # 'PrompterInfo     - some informations (version, commands
