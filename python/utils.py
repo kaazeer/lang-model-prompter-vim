b24d59a4e44f2d9a9979f2835b0e673f0ea4f19c
kaazeer <122846426+kaazeer@users.noreply.github.com>
2021-06-23 11:44:08 +0000

    Delete lib

--- python/utils.py
+++ python/utils.py
@@ -28,4 +28,4 @@ def model_settings(
     max_tokens: int,
     stop: [str],
 ) -> str:
-    ''' print the model configuration paramet
+    ''' print the model configuration parameters, in one line 
