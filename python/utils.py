b4b849921e8a320d8407c703c299950c6692e345
kaazeer <122846426+kaazeer@users.noreply.github.com>
2021-08-19 15:36:36 +0000

    Changed project name

--- python/utils.py
+++ python/utils.py
@@ -63,4 +63,4 @@ def completion_statistics(
     prompt_tokens = usage['prompt_tokens']
     total_tokens = usage['total_tokens']
 
-    tokens_report = f'{total
+    tokens_report = f'{total_tokens} (prompt: {prompt_t
