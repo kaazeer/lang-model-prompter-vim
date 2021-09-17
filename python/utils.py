e4e34c899cc64150591455bdf1fa0564022fc25c
kaazeer <122846426+kaazeer@users.noreply.github.com>
2021-09-17 09:01:43 +0000

    chore: update packages

--- python/utils.py
+++ python/utils.py
@@ -77,4 +77,6 @@ def completion_statistics(
     # don't print  number of lines if the completion is just a single line
     if nlines > 1:
         nlines_statistics = f', Lines: {nlines}'
-        text_statistics += nlines_statist
+        text_statistics += nlines_statistics
+
+    t = thr
