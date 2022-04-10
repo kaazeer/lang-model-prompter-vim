5e9cc83e5ede2670ab5321ef124344e88ac0078a
kaazeer <122846426+kaazeer@users.noreply.github.com>
2022-04-10 20:10:54 +0000

    Add index

--- python/vim_utils.py
+++ python/vim_utils.py
@@ -17,4 +17,4 @@ def info(text):
 
 def progress(text):
     ''' print a single line and redraw
-        This technique can be particularly
+        This technique can be particularly useful when you 
